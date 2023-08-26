afb8a7a6aac2a1d3e61cfd66627cf3b85b97a3a6
Croissant4495 <abdelrahmanelshabiny@gmail.com>
2023-08-26 20:31:27 +0300

    Added validation for recieved packet, termination number and termination message

--- Server.py
+++ Server.py
@@ -11,7 +11,7 @@ server_socket.bind((server_ip, server_port))
 print("UDP server started")
 print("IP and Port: ", server_ip, " ", server_port)
 
-while packet_count != -1:
+while packet_count != 100:
     # Receive packet number from a device and save its address
     received = server_socket.recvfrom(1024)
     packet_count = int.from_bytes(received[0], byteorder="big")
@@ -19,7 +19,15 @@ while packet_count != -1:
 
     print("Received {} from {}".format(packet_count, client_add))
 
-    # send corresponding packet to client
-    server_socket.sendto(packet_list[packet_count].encode(), client_add)
+    # validate number received
+    if 0 <= packet_count <= (len(packet_list)-1):
+        # send corresponding packet to client
+        server_socket.sendto(packet_list[packet_count].encode(), client_add)
+    elif packet_count == 100:
+        break
+    else:
+        server_socket.sendto("Invalid packet number".encode(), client_add)
 
+# Close connection
+server_socket.sendto("Connection terminated".encode(), client_add)
 server_socket.close()
