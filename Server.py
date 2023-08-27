2637a12297b9cd42094789f666ecf6db0d5efbf1
mohamedashraf18 <mohamedprins19@gmail.com>
2023-08-27 09:35:02 +0300

    made the client file and made the number of the packet list's elements be 4 instead of a 100

--- Server.py
+++ Server.py
@@ -8,22 +8,25 @@ server_port = 12347
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_socket.bind((server_ip, server_port))
+
 print("UDP server started")
 print("IP and Port: ", server_ip, " ", server_port)
 
-while packet_count != 100:
+while packet_count != 4:
+    
     # Receive packet number from a device and save its address
     received = server_socket.recvfrom(1024)
+
     packet_count = int.from_bytes(received[0], byteorder="big")
     client_add = received[1]
 
-    print("Received {} from {}".format(packet_count, client_add))
+    print( "Received {} from {}".format(packet_count, client_add) )
 
     # validate number received
     if 0 <= packet_count <= (len(packet_list)-1):
         # send corresponding packet to client
         server_socket.sendto(packet_list[packet_count].encode(), client_add)
-    elif packet_count == 100:
+    elif packet_count == 4:
         break
     else:
         server_socket.sendto("Invalid packet number".encode(), client_add)
